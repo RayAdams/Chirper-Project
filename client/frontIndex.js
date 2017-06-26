a7944eb29f18c508b3866832033203347fd666f1
RayAdams <cramerrachael@gmail.com>
2017-06-25 22:22:09 -0400

    added files, ready to add routes

---
+++ client/frontIndex.js
@@ -0,0 +1,95 @@
+var $chirpButton = $('#chirp-btn');
+var $chirpField = $('#chirp-field');
+var $chirpList = $('#chirp-list');
+var $userSelector = $('#user-selector');
+
+$chirpButton.click(postChirp);
+
+//chirp button disable logic 
+ $chirpField.on('input', function(){
+    var isEmpty = $chirpField.val().length === 0;
+        $chirpButton.prop('disabled', isEmpty);
+ });
+
+function postChirp(){
+    var chirp = {
+        userid: $userSelector.val(),
+        message: $chirpField.val()
+    };
+    $.ajax({
+        method: 'POST',
+        url: '/api/chirps',
+        contentType: 'application/json',
+        data: JSON.stringify(chirp)
+    }).then(function(success) {
+        $chirpField.val('');
+        $chirpButton.prop('disabled', true);
+        getChirps();
+    }, function(err) {
+        console.log(err);
+    });
+}
+
+function getChirps() {
+    $.ajax({
+        method: 'GET',
+        url: '/api/chirps'
+    }).then(function(chirps) {
+        $chirpList.empty();
+        for (var i = 0; i < chirps.length; i++) {
+            addChirpDiv(chirps[i]);
+        }
+    }, function(err) {
+        console.log(err);
+    });
+}
+getChirps();
+
+function addChirpDiv(chirp) {
+var $chirpDiv = $('<div class="chirp"></div>');
+    var $user = $('<h4></h4>');            
+    var $message = $('<p></p>');
+    var $timestamp = $('<h5></h5>');
+    var $deleteBtn = $('<button class="btn btn-danger btn-xs delete"><i class="fa fa-trash-o"></i>DELETE</button>');
+    $deleteBtn.click(function() {
+        deleteChirp(chirp.id);
+    });
+
+    $user.text(chirp.userName);
+    $message.text(chirp.message);
+    $timestamp.text(new Date(chirp.timestamp).toLocaleString());
+
+    $user.appendTo($chirpDiv);
+    $message.appendTo($chirpDiv);
+    $timestamp.appendTo($chirpDiv);
+    $deleteBtn.appendTo($chirpDiv);
+
+    $chirpDiv.appendTo($chirpList);
+}
+
+function deleteChirp(id) {
+    $.ajax({
+        method: 'DELETE',
+        url: '/api/chirps/' + id
+    }).then(function() {
+        getChirps();
+    }, function(err) {
+        console.log(err);
+    });
+}
+
+function populateUsers() {
+    $.ajax({
+        method: 'GET',
+        url: '/api/users'
+    }).then(function(users) {
+        for(var i = 0; i < users.length; i++) {
+            var $userOption = $('<option value="' + users[i].id + '">' + users[i].name + '</option>');
+            $userSelector.append($userOption);
+        }
+    }, function(err) {
+        console.log(err);
+    });
+}
+
+populateUsers();
---
+++ client/single_update.js
@@ -0,0 +1,7 @@
+// * Tell Express to send this file when it receives a GET request to `'/chirps/*/update'`
+//     * This page will contain an input box for the message of the chirp
+//     * It will also display the chirp author's name, but it will not be in an editable format
+//     * When the page loads, look at the url and figure out which chirp to GET from the API
+//     * Populate the message input box with the text of the chirp returned from the server
+//     * After a user changes the message, clicking the Update button will cause an API call to update the chirp
+//         * When that is successful, navigate back to the single chirp page for this chirp
